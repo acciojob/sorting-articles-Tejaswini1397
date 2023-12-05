2b3cf30f4a9a002e4058d18beeae6dee43aa7b87
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-12-05 18:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-//your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function () {
-    const articles = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-    // Sort the articles while ignoring "a," "an," and "the"
-    const sortedArticles = articles.sort((a, b) => {
-        const wordsToIgnore = ["a", "an", "the"];
-        const getTitle = article => article.toLowerCase().replace(/^(a|an|the)\s+/i, '');
-        return getTitle(a).localeCompare(getTitle(b));
-    });
+const sortedBands = bands.sort((a, b) => {
+	const ignoreWords = ['a', 'an', 'the'];
+	const removeIgnoredWords = (str) => str.replace(/^(a|an|the) /i, '');
+	return removeIgnoredWords(a).localeCompare(removeIgnoredWords(b));
+});
 
-    // Display sorted articles in the webpage
-    const articlesList = document.getElementById('articles');
-    sortedArticles.forEach(article => {
-        const listItem = document.createElement('li');
-		
-        listItem.textContent = article;
-        articlesList.appendChild(listItem);
-    });
+const bandList = document.getElementById('bands');
+sortedBands.forEach(article => {
+	const listItem = document.createElement('li');
+	listItem.textContent = article;
+	bandList.appendChild(listItem);
 });
